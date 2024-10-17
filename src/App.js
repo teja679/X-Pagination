b542a600ddd590ca411b2ff625fa8f0a5e575185
tejaabp <tejaabp@gmail.com>
2024-10-18 00:49:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import LandingPage from './components/LandingPage';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [employees, setEmployees] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchData = async () => {
+    const response = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+    const jsonResponse = await response.json();
+    setEmployees(jsonResponse)
+  }
+  const handleChange = (val) => {
+    let len = employees.length;
+    if ((val === 1 && page < 10 && (page * 10 < len)) || (val === -1 && page > 1)) {
+      setPage(prev => prev + val)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Employee Data Table</h1>
+      <LandingPage employees={employees} page={page} />
+      <div className='buttons-div'>
+        <button onClick={() => handleChange(-1)} className='prev-btn'>Previous</button>
+        <p className='page-no'>{page}</p>
+        <button onClick={() => handleChange(1)} className='next-btn'>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import './styles.css'
+
+const LandingPage = ({ employees, page }) => {
+    const [fiteredEmployees, setFiteredEmployees] = useState([]);
+    const filterData = (page) => {
+        const start = ((page - 1) * 10) + 1;
+        const end = (page * 10);
+        const arrayData = employees.filter((item, index) => index + 1 >= start && index + 1 <= end);
+        setFiteredEmployees(arrayData);
+    }
+
+    useEffect(() => {
+        filterData(page)
+    }, [page])
+
+    return (
+        <div className='landing-page'>
+            <table>
+                <thead>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                </thead>
+                <tbody>
+                    {fiteredEmployees &&
+                        fiteredEmployees.map(employee =>
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>{employee.name}</td>
+                                <td>{employee.email}</td>
+                                <td>{employee.role}</td>
+                            </tr>
+
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default LandingPage
